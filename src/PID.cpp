130384f55207acd70caac7281210fccbd2d0979a
atif-hussain <atif.register@gmail.com>
2017-07-18 17:16:45 +0530

    Good commit with full round lap drive

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,27 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+   p_error = 0.0;
+   i_error = 0.0;
+   d_error = 0.0;
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double _Kp, double _Ki, double _Kd) {
+   Kp = _Kp;
+   Ki = _Ki;
+   Kd = _Kd;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = (cte - p_error);
+    i_error += cte;
+    p_error = cte;
 }
 
 double PID::TotalError() {
+	return - Kp * p_error - Ki * i_error - Kd * d_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,19 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  // Tweak initial Kp, Ki, Kd values.
+	// Manual tweaking. start with kp=kd=ki=0.1 for 10 turns, ie. 0.1, .01, 1.0 
+	// observed behaviour, slight bias to right, negligible, ignored
+	// observed behaviour, lot of wobbling, so increase kd significantly kd=3
+	// observed behaviour, driving too slow slowing down simulation, so increase throttle 0.3 to 0.8
+	// with higher throttle, still more wobbling. kd=8
+	// TODO: additionally, change based on drive speed you want
+  double Kp0 = 0.1;
+  double Ki0 = .0005;
+  double Kd0 = 2.0;
+  // std::cin >> Kp0 >> Ki0 >> Kd0; make it flexible to tweak & test
+  pid.Init(Kp0, Ki0, Kd0);
+
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,15 +70,18 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+		  if (abs(steer_value)>1) steer_value = (steer_value >= 0) * 1.0;
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = 0.3;	
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
